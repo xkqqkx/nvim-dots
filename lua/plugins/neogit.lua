9bdb49963b741747cc7ca8029a5c11b4493329e2
Marek <marek@biolojic.com>
2025-11-02 00:48:09 +0200

    fix: troubleshooting neogit and diffview
    the problem was outdated version of git

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,11 +3,12 @@ return   {
     'NeogitOrg/neogit',
     dependencies = {
       'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
+      "sindrets/diffview.nvim",
 
       -- Only one of these is needed, not both.
       -- 'nvim-telescope/telescope.nvim',
       'ibhagwan/fzf-lua', -- optional
     },
-    config = true,
+    integrations = {diffview=true},
+    -- config = true,
 }
