5302143d3e6d037f82157068c9b511761638a57a
Marek <marek@biolojic.com>
2025-12-06 02:26:46 +0200

    refactor: consolidated markdown related into single lua config file

--- lua/plugins/glow.lua
+++
@@ -1,3 +0,0 @@
-return {
-    { 'ellisonleao/glow.nvim', config = true, widht=120, width_ratio = 0.9, style = 'light', border = 'single', cmd = 'Glow' },
-}
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {
+    -- markdown formatting inside buffer
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+
+    -- markdown preview using Glow
+    { 'ellisonleao/glow.nvim', config = true, widht=120, width_ratio = 0.9, style = 'light', border = 'single', cmd = 'Glow' },
+}
